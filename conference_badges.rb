f998105a754d5da838464dafae9b7fe5b2b862a5
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 14:08:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(arr)
   arr.each_with_index do |name, index|
-    puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+   "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
 
